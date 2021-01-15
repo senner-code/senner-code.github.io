b73c65fa6485e993b155e0d5c10c0b6f4e087fa4
Senner <senner.mail@gmail.com>
2021-01-15 13:38:31 +0200

    Test JS Code

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
+const person = {
+    name:"Senner",
+    age: 17,
+    isProgrammer: true,
+    l: ["ru", "en", "de"],
+    hello(){
+        console.log("hello world")
+    }
+}
+const logger = {
+    keys() {
+        console.log("Object Keys: ", Object.keys(this))
+    },
+
+    keyAndValues() {
+        Object.keys(this).forEach((key) => {
+            console.log(key ,":", this[key])
+        })
+    }
+
+}
+
+logger.keyAndValues.call(person)
+
+
+
 
 
 const a = document.getElementById("hello")
 
-
-const b = a.textConte
-          nt
-a.textContent = "Name"
+a.textContent = logger.keyAndValues.call(person)
