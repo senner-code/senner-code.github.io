5434efa6389fe63830da2c9d9092826abe68355f
Vadim Popov <65708779+senner-code@users.noreply.github.com>
2021-02-14 12:25:53 +0200

    commit

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-const card = $.card(
-    [{
-            id: 0,
-            title: "Уравнение",
-            holder: "Введите число",
-            fun: value => {
-                return value ** 2
-            }
-        },
+const insertNumber = (options) => {
+    const form = document.querySelector('.insert')
+    form.insertAdjacentHTML('afterbegin', `
+    <input type="text" placeholder="${options.first + 1} X ${options.second + 1}" data-row="${options.first}" data-column="${options.second}">`)
+}
 
-        {
-            id: 1,
-            title: "Пример",
-            holder: "Введите пример",
-            fun: value => {
-                return Math.sqrt(value)
-            }
-        },
+const arrayCreate = (rows, coll) => {
+    let massive = new Array()
+    for (let i = 0; i < rows; i++) {
+        massive[i] = new Array()
+        for (let j = 0; j < coll; j++) {
+            massive[i][j] =0
+            insertNumber({
+                first: i,
+                second: j
+            })
+        }
+    }
+    confirmButton.style.zIndex = 1
+    return massive
+}
 
-        {
-            id: 2,
-            title: "Задача",
-            holder: "Введите число",
-            fun: value => {
-                return Math.sqrt(value) + 1
+const listener = (event) => {
+    if (event.target.dataset.massive) {
+        let nm = document.querySelectorAll('.nm')
+        arrayCreate(nm[0].value, nm[1].value)
+    }
+}
+
+
+const el = document.querySelector('[data-massive]')
+el.addEventListener('click', listener)
+
+const form = document.querySelector('.confirm-block')
+form.insertAdjacentHTML('afterbegin', `<button class="main">Поиск нулей</button>`)
+
+const confirmButton = document.querySelector('.main')
+
+const hello = () => {
+    let td = document.querySelectorAll('[data-row]')
+    let nm = document.querySelectorAll('.nm')
+    let massive = new Array()
+    indexx = 0
+    for (let i = 0; i < nm[0].value; i++) {
+        massive[i] = new Array()
+        for (let j = 0; j < nm[1].value; j++) {
+            massive[i][j] = td[indexx].value
+            indexx++
+        }
+    }
+    let zero = 0
+    for (let p = 0; p < massive.length; p++) {
+        for (let g = 0; g < massive[p].length; g++) {
+            console.log(massive[p][g])
+            if(massive[p][g] == 0){
+                zero++
             }
-        },
-    ]
-)
+        }
+    }    
+    alert("НУЛЕЙ - "+ zero)
+}
+
+
+confirmButton.onclick = function(){hello()}
